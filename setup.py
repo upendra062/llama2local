1fcda12c65d38d2d9e475e84d6c906beb139f434
upendra <bhadauriaupendra062@gmail.com>
2024-04-29 13:20:55 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI Project',
+    version= '0.0.0',
+    author= 'Rocky',
+    author_email= 'bhadauriaupendra062@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+)
